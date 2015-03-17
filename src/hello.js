f935a757b8832c7b6b055e7a60dfc47e53f1ba30
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2015-03-17 01:04:46 +0100

    feat: set content into iframe

--- src/hello.js
+++ src/hello.js
@@ -6,8 +6,8 @@ import {EditorIframe} from 'editorIframe';
     selector: 'editor-app'  //TODO: default to camel-cased class name if not provided?
 })
 @Template({
-    inline: `<ace-editor style="width:100%; height:100%; display:block;"></ace-editor>
-            <button (click)="editor.setContent()"">foo</button>
+    inline: `<ace-editor #ace style="width:100%; height:100%; display:block;"></ace-editor>
+            <button (click)="editor.setContent(ace.getContent())"">foo</button>
             <editor-iframe #editor></editor-iframe>`,
     directives: [AceEditor, EditorIframe]
 })
