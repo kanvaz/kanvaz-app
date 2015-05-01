05abdda917c6648e9db22b8e950dae2341275221
Pascal Precht <pascal.precht@googlemail.com>
2015-05-01 13:21:56 +0200

    feat(defaultKanvaz): introduce defaultKanvaz service
    This allows us to scaffold a brand new kanvaz

--- src/services/KanvazService.js
+++ src/services/KanvazService.js
@@ -1,37 +1,12 @@
 import { PromiseWrapper } from 'angular2/src/facade/async';
 import { Kanvaz } from 'kanvaz';
-
+import { defaultKanvaz } from 'default-kanvaz';
 
 export class KanvazService {
 
   create() {
     let completer = PromiseWrapper.completer();
-
-    // TODO(pascal): extract kanvaz data
-    completer.resolve(new Kanvaz([
-      {
-        name: 'index.html',
-        content: `<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Kanvaz</title>
-  </head>
-  <body>
-    <h1>Hello</h1>
-  </body>
-</html>`
-      },
-      {
-        name: 'styles.css',
-        content: 'h1 { color: red; }'
-      },
-      {
-        name: 'app.js',
-        content: ''
-      }
-    ]));
-
+    completer.resolve(defaultKanvaz);
     return completer.promise;
   }
 }
